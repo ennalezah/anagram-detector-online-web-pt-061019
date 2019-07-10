b5359227bf1c95fd88386df301d291e2d7e90e31
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-10 01:55:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,14 +1,16 @@
 # Your code goes here!
 
 class Anagram
-  attr_accessor :word
+  attr_accessor :given_word
   
-  def initialize(word)
-    @word = word
+  def initialize(given_word)
+    @given_word = give_word
   end
   
   def match(possible_anagrams)
     # return all words in possible_anagrams that match with word
+    
+    possible_anagrams.find {|word| }
     
     # return empty array if no match
   end
