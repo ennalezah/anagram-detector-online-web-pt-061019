254d4ff864e8f8489f2ed9c3af525c34406b22bf
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-10 02:52:01 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,16 +8,8 @@ class Anagram
   end
   
   def match(possible_anagrams)
-    # return all words in possible_anagrams that match with word
-    
     split_input = input.split("")
     
     possible_anagrams.select {|word| word.split("").sort == split_input.sort}
-    
-  # if has_match != true
-  #   []
-  # end
-    
-    # return empty array if no match
   end
 end 
